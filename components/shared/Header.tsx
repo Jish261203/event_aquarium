a4eea502b824427682a503f14b8f966d8334999e
Your Name <jish261203@gmail.comgit config --global user.name Jishnay>
2024-02-15 00:29:49 +0530

    Implemented RBAC

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,14 +1,18 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
 
+
+
 const Header = () => {
+
+
 	return (
 		<header className="w-full border-b">
-			<div className="wrapper flex item-center justify-between">
+			<div className="wrapper flex items-center justify-between">
 				<Link href="/" className="w-36">
 					<Image
 						src="/assets/images/logo1.svg"
@@ -30,7 +34,7 @@ const Header = () => {
 					<SignedOut>
 						<Button
 							asChild
-							className="rounded-full  bg-blue-500 text-white border-blue-500 hover:bg-white hover:text-blue-500 transition-colors duration-300"
+							className="rounded-full bg-blue-500 text-white border-blue-500 hover:bg-white hover:text-blue-500 transition-colors duration-300"
 							size="lg">
 							<Link href="/sign-in">Login</Link>
 						</Button>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 
+
 import { twMerge } from "tailwind-merge";
 import qs from "query-string";
 
@@ -103,3 +104,4 @@ export const handleError = (error: unknown) => {
 	console.error(error);
 	throw new Error(typeof error === "string" ? error : JSON.stringify(error));
 };
+
